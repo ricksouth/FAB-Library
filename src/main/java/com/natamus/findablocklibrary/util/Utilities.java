620f3c5a14793533bac575ea8b80798ae03ddd2b
Rick South <rick@natamus.com>
2020-04-17 17:28:45 +0200

    Updated to 1.0.

--- src/main/java/com/natamus/findablocklibrary/util/Utilities.java
+++ src/main/java/com/natamus/findablocklibrary/util/Utilities.java
@@ -8,56 +8,62 @@ import java.util.Map;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class Utilities {
-	private static Map<World, List<BlockPos>> worldcampfires = new HashMap<World, List<BlockPos>>();
+	private static Map<Block, Map<World, List<BlockPos>>> getMapFromBlock = new HashMap<Block, Map<World, List<BlockPos>>>();
 	private static Map<World, Map<Date, BlockPos>> timeoutpositions = new HashMap<World, Map<Date, BlockPos>>();
 	
 	public static BlockPos getRequestedBlockAroundEntitySpawn(Block requestedblock, Integer radius, Double radiusmodifier, World world, Entity entity) {
+		Map<World,List<BlockPos>> worldblocks;
+		if (getMapFromBlock.containsKey(requestedblock)) {
+			worldblocks = getMapFromBlock.get(requestedblock);
+		}
+		else {
+			worldblocks = new HashMap<World, List<BlockPos>>();
+		}
+		
 		List<Entity> passengers = entity.getPassengers();
 		BlockPos epos = entity.getPosition();
 		
-		List<BlockPos> currentcampfires;
-		BlockPos removetodatescampfire = null;
+		List<BlockPos> currentblocks;
+		BlockPos removeblockpos = null;
 		
-		if (worldcampfires.containsKey(world)) {
-			currentcampfires = worldcampfires.get(world);
+		if (worldblocks.containsKey(world)) {
+			currentblocks = worldblocks.get(world);
 			
-			List<BlockPos> cftoremove = new ArrayList<BlockPos>(); 
-			for (BlockPos campfire : currentcampfires) {
-				if (!world.getChunkProvider().chunkExists(campfire.getX() >> 4, campfire.getZ() >> 4)) {
-					cftoremove.add(campfire);
+			List<BlockPos> cbtoremove = new ArrayList<BlockPos>(); 
+			for (BlockPos cblock : currentblocks) {
+				if (!world.getChunkProvider().chunkExists(cblock.getX() >> 4, cblock.getZ() >> 4)) {
+					cbtoremove.add(cblock);
 					continue;
 				}
-				if (!world.getBlockState(campfire).getBlock().equals(Blocks.CAMPFIRE)) {
-					cftoremove.add(campfire);
+				if (!world.getBlockState(cblock).getBlock().equals(requestedblock)) {
+					cbtoremove.add(cblock);
 					continue;
 				}
 				
-				if (campfire.withinDistance(epos, radius*radiusmodifier)) {
+				if (cblock.withinDistance(epos, radius*radiusmodifier)) {
 					if (passengers.size() > 0) {
 						for (Entity passenger : passengers) {
 							passenger.remove();
 						}
 					}
-					System.out.println("Campfire saved!");
-					removetodatescampfire = campfire.toImmutable();
-					return campfire.toImmutable();
+					removeblockpos = cblock.toImmutable();
+					return cblock.toImmutable();
 				}
 			}
 			
-			if (cftoremove.size() > 0) {
-				for (BlockPos tr : cftoremove) {
-					currentcampfires.remove(tr);
+			if (cbtoremove.size() > 0) {
+				for (BlockPos tr : cbtoremove) {
+					currentblocks.remove(tr);
 				}
 			}
 		}
 		else {
-			currentcampfires = new ArrayList<BlockPos>();
+			currentblocks = new ArrayList<BlockPos>();
 		}
 		
 		// Timeout function which prevents too many of the loop through blocks.
@@ -70,21 +76,17 @@ public class Utilities {
 				Date now = new Date();
 				for (Date todate : timeouts.keySet()) {
 					BlockPos toepos = timeouts.get(todate);
-					if (removetodatescampfire != null) {
-						if (toepos.withinDistance(removetodatescampfire, 64)) {
+					if (removeblockpos != null) {
+						if (toepos.withinDistance(removeblockpos, 64)) {
 							totoremove.add(todate);
-							System.out.println("TOTO removed campfire.");
 						}
 					}
 					long ms = (now.getTime()-todate.getTime())/1000;
-					System.out.println(ms);
 					if (ms > 20000) {
 						totoremove.add(todate);
-						System.out.println(todate.toString() + " expired!!");
 						continue;
 					}
 					if (toepos.withinDistance(epos, radius*radiusmodifier)) {
-						System.out.println("Timeout shows entity nearby. time and resources saved!");
 						return null;
 					}
 				}
@@ -100,18 +102,16 @@ public class Utilities {
 			timeouts = new HashMap<Date, BlockPos>();
 		}
 		
-		System.out.println(currentcampfires);
-		System.out.println(timeouts);
-		
 		int r = radius;
 		for (int x = -r; x < r; x++) {
 			for (int y = -r; y < r; y++) {
 				for (int z = -r; z < r; z++) {
 					BlockPos cpos = epos.east(x).north(y).up(z);
 					BlockState state = world.getBlockState(cpos);
-					if (state.getBlock().equals(Blocks.CAMPFIRE)) {
-						currentcampfires.add(cpos.toImmutable());
-						worldcampfires.put(world, currentcampfires);
+					if (state.getBlock().equals(requestedblock)) {
+						currentblocks.add(cpos.toImmutable());
+						worldblocks.put(world, currentblocks);
+						getMapFromBlock.put(requestedblock, worldblocks);
 						
 						if (passengers.size() > 0) {
 							for (Entity passenger : passengers) {
